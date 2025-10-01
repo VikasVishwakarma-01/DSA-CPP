11745b08bbd7abf004a9b9b70a220de41eedf6fb
Vikas Vishwakarma <vikasweb77@gmail.com>
2025-10-01 16:14:25 +0530

    Time: 0 ms (100%), Space: 8 MB (36.81%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -3,8 +3,9 @@ public:
     int countVowelStrings(int n) {
         if(n == 1) return 5;
         vector<int> v = {5, 4, 3, 2, 1};
-        int sum = 0;
+        
         for(int i = 0; i < n-2; i++) {
+            int sum = 0;
             for(int k = 0; k < 5; k++) sum += v[k];
             vector<int> temp(5);
             temp[0] = sum;
@@ -12,9 +13,9 @@ public:
                 temp[j] = temp[j-1] - v[j-1];
             }
             v = temp;
-            sum = 0;
         }
-        sum = 0;
+
+        int sum = 0;
         for(int i = 0; i < v.size(); i++) {
             sum += v[i];
         }
