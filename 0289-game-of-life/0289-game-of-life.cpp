4d9daafffe059799529c820a4d4362a895c91c7c
Vikas Vishwakarma <vikasweb77@gmail.com>
2026-04-13 16:48:36 +0530

    Time: 0 ms (100%), Space: 9.8 MB (6.82%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    int solve(int cell, int i, int j, vector<vector<int>>& board) {
+    vector<vector<int>> dir = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    int solve(int cell, int x, int y, vector<vector<int>>& board) {
        
         int n = board.size();
         int m = board[0].size();
 
         int cnt = 0;
-        if(i > 0 && j > 0) 
-            cnt += board[i-1][j-1];
-        if(j > 0) 
-            cnt += board[i][j-1];
-        if(i < n-1 && j > 0) 
-            cnt += board[i+1][j-1];
-        if(i > 0) 
-            cnt += board[i-1][j];
-        if(i < n-1) 
-            cnt += board[i+1][j];
-        if(i > 0 && j < m-1) 
-            cnt += board[i-1][j+1];
-        if(j < m-1) 
-            cnt += board[i][j+1];
-        if(i < n-1 && j < m-1) 
-            cnt += board[i+1][j+1];
-
+        for(auto &d: dir) {
+            int i = x + d[0];
+            int j = y + d[1];
+            if(i >= 0 && i < n && j >= 0 && j < m) 
+                cnt += board[i][j];
+        }
 
         if(cell == 1) {
             if(cnt < 2 or cnt > 3)
