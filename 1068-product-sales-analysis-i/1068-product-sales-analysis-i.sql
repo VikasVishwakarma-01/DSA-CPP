b6f08c1e20b30d1afdc05ad869f4c9c927769fe3
Vikas Vishwakarma <vikasweb77@gmail.com>
2026-05-23 13:06:19 +0530

    Time: 1423 ms (17.48%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,6 +1,4 @@
-SELECT  p.product_name, 
-        s.year AS 'year', 
-        s.price
+SELECT p.product_name, s.year, s.price
 FROM Sales s
-LEFT JOIN Product p
+JOIN Product p
 ON s.product_id = p.product_id;
