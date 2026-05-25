b71328445aa1f323b14ef79b90eb2c8f6aac69be
Vikas Vishwakarma <vikasweb77@gmail.com>
2026-05-25 15:27:01 +0530

    Time: 522 ms (6.36%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,10 @@
-SELECT name 
-FROM Employee 
-WHERE id IN (
-    SELECT managerId 
-    FROM Employee 
-    GROUP BY managerId 
-    HAVING COUNT(*) >= 5)
+WITH managers(managerId) AS (
+    SELECT managerId
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(id) >= 5
+)
+
+SELECT name
+FROM Employee
+WHERE id IN (SELECT managerId FROM managers);
