0df4f88a1167fa7e73d4020a1be5a239bed6141d
Vikas Vishwakarma <vikasweb77@gmail.com>
2026-05-26 12:27:37 +0530

    Time: 2 ms (35.5%), Space: 9.6 MB (27.15%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -10,7 +10,6 @@ public:
         int cnt = 0;
         for(auto x: word) {
             if(x >= 97 && x <= 122) {
-                
                 char ch = toupper(x);
                 if(mp.find(ch) != mp.end()) {
                     cnt++;
